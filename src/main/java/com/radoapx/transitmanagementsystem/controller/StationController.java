290da4c294e203aaf6939951d2a8728bb5aa6cf8
radoapx <845225343@qq.com>
2019-06-25 19:01:42 +0800

    获取所有点点

--- src/main/java/com/radoapx/transitmanagementsystem/controller/StationController.java
+++ src/main/java/com/radoapx/transitmanagementsystem/controller/StationController.java
@@ -1,12 +1,22 @@
 package com.radoapx.transitmanagementsystem.controller;
 
+import com.radoapx.transitmanagementsystem.entity.StationEntity;
 import com.radoapx.transitmanagementsystem.jpa.StationJPA;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(path = "/get")
 public class StationController {
     @Autowired
     private StationJPA stationJPA;
+
+    @RequestMapping(path = "/getallpoints")
+    public List<StationEntity> getALLPoints(){
+        return stationJPA.findAll();
+    }
 }
